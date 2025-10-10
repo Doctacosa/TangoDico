01b2488985028965dbfe4c4b8d01e8d61de2da8a
Stéphane Lemay <interordi@interordi.com>
2025-10-09 23:55:44 -0400

    Set proper dev and build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+//import vueDevTools from 'vite-plugin-vue-devtools'
+
+let outDir = 'C:/xampp/htdocs/tango.interordi.com/';
+if (process.env.OS === 'Windows_NT') {
+	//Nothing, it's the default value
+} else if (process.platform === 'darwin')
+	outDir = '/opt/homebrew/var/www/tango.interordi.com/';
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,9 +17,16 @@ export default defineConfig({
 		vue(),
 		//vueDevTools(),	//Enable for devtools support
 	],
+	build: {
+		outDir: outDir,
+		emptyOutDir: true,
+	},
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url))
 		},
 	},
+	server: {
+		port: 5175,
+	}
 })
