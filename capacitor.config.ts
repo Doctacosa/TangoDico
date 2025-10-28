02b170f69b171081977300111c37c2087772d4df
Stephane Lemay <interordi@interordi.com>
2025-10-27 22:59:27 -0400

    Add Capacitor for iOS

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.interordi.tango',
+  appName: 'Tango Dico',
+  webDir: '/opt/homebrew/var/www/tango.interordi.com/'
+};
+
+export default config;
